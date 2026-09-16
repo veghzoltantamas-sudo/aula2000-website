2acf04d66f10a86c106e9ba46b2473e52b8ddf79
Aula2000 Dev <dev@aula2000.hu>
2026-09-16 09:52:42 +0000

    fix: CSRF token - res.locals helyett req.csrfToken elhavitas

--- routes/public.js
+++ routes/public.js
@@ -321,7 +321,6 @@ function createPublicRouter(deps) {
                 comments,
                 commentMsg: req.query.msg,
                 currentUrl: req.protocol + '://' + req.get('host') + req.originalUrl,
-                csrfToken: req.csrfToken(),
                 escapeHtml 
             });
         } catch (err) { next(err); }
